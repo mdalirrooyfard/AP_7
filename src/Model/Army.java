b90d66f03e0bcca93269594d39c3eb20b9337a0d
ZahraFazel <zfazel1379@gmail.com>
2018-10-25 16:47:29 +0330

    Army is ready!

--- src/Model/Army.java
+++ src/Model/Army.java
@@ -1,4 +1,45 @@
 package Model;
 
-public class Army {
+public class Army extends Item
+{
+    int level , numberOfEmployee , blockId , unitId;
+    double powerLevel;
+    boolean existance;
+    final int UPGRADE_COST = -20000;
+    final int ADD_COST = -15000;
+    final int REMOVE_COST = 10000;
+
+    public int getBlockId()
+    {
+        return blockId;
+    }
+    public int getUnitId()
+    {
+        return unitId;
+    }
+    public int getUPGRADE_COST()
+    {
+        return UPGRADE_COST;
+    }
+    public int getADD_COST()
+    {
+        return ADD_COST;
+    }
+    public int REMOVE_COST()
+    {
+        return REMOVE_COST;
+    }
+    public Army add( int blockId , int unitId )
+    {
+        if( !existance )
+        {
+            existance = true;
+            level = 1;
+            numberOfEmployee = 100;
+            this.blockId = blockId;
+            this.unitId = unitId;
+            powerLevel = 0.2;
+        }
+        return this;
+    }
 }
