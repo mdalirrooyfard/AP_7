90a9d6e6a668756669f08e6b72618a73842d6a8e
ZahraFazel <zfazel1379@gmail.com>
2018-12-01 17:04:38 +0330

    "Cell" fields are created.

--- src/Model/Cell.java
+++ src/Model/Cell.java
@@ -1,4 +1,27 @@
 package Model;
 
-public class Cell {
+import java.util.ArrayList;
+
+public class Cell
+{
+    private ArrayList<Entity> stuffs = new ArrayList<>();
+
+    Cell( ArrayList<Entity> stuffs )
+    {
+        this.stuffs = stuffs;
+    }
+
+    public ArrayList<Entity> getStuffs()
+    {
+        return stuffs;
+    }
+    public void add(Entity newStuff)
+    {
+        stuffs.add(newStuff);
+    }
+    public void remove(Entity stuff)
+    {
+        stuffs.remove(stuff);
+    }
+
 }
