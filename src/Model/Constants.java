4a62a0f94c735bbe7f3d5e56c7dfab48ca0e853c
ZahraFazel <zfazel1379@gmail.com>
2018-12-02 16:15:52 +0330

    "Constants" is created.

---
+++ src/Model/Constants.java
@@ -0,0 +1,69 @@
+package Model;
+
+public class Constants
+{
+    private final int EGG_VOLUME = 1;
+    private final int WOOL_VOLUME = 5;
+    private final int MILK_VOLUME = 10;
+    private final int EGG_POWDER_VOLUME = 4;
+    private final int COOKIE_VOLUME = 5;
+    private final int CAKE_VOLUME = 6;
+    private final int STRING_VOLUME = 3;
+    private final int CLOTH_VOLUME = 6;
+    private final int CARNIVAL_DRESS_VOLUME = 8;
+    private final int FLOUR_VOLUME = 1;
+    private final int ADORNMENT_VOLUME = 4;
+    private final int PLUMRE_VOLUME = 2;
+    private final int CAGED_BROWN_BEAR_VOLUME = 20;
+    private final int CAGED_LION_VOLUME = 20;
+
+    private final int EGG_BUY_COST = 20;
+    private final int WOOL_BUY_COST = 200;
+    private final int MILK_BUY_COST = 2000;
+    private final int EGG_POWDER_BUY_COST = 50;
+    private final int COOKIE_COST = 200;
+    private final int CAKE_BUY_COST = 400;
+    private final int STRING_BUY_COST = 300;
+    private final int CLOTH_BUY_COST = 400;
+    private final int CARNIVAL_DRESS_BUY_COST = 1400;
+    private final int FLOUR_BUY_COST = 20;
+    private final int CAGED_BROWN_BEAR_BUY_COST = 100;
+    private final int CAGED_LION_BUY_COST = 150;
+
+    private final int EGG_SALE_COST = 10;
+    private final int WOOL_SALE_COST = 100;
+    private final int MILK_SALE_COST = 1000;
+    private final int EGG_POWDER_SALE_COST = 50;
+    private final int COOKIE_SALE_COST = 100;
+    private final int CAKE_SALE_COST = 200;
+    private final int STRING_SALE_COST = 150;
+    private final int CLOTH_SALE_COST = 300;
+    private final int CARNIVAL_DRESS_SALE_COST = 1300;
+    private final int FLOUR_SALE_COST = 10;
+    private final int ADORNMENT_SALE_COST = 300;
+    private final int CAGED_BROWN_BEAR_SALE_COST = 100;
+    private final int CAGED_LION_SALE_COST = 150;
+
+    /*
+    <Horn DepotSize="6" BuyCost="2000" SaleCost="1000"/>
+    <Plume DepotSize="2" BuyCost="200" SaleCost="100"/>
+    <SourCream DepotSize="8" BuyCost="3000" SaleCost="1500"/>
+    <Curd DepotSize="6" BuyCost="4000" SaleCost="2000"/>
+    <Cheese DepotSize="5" BuyCost="5000" SaleCost="2500"/>
+    <ColoredPlume DepotSize="2" BuyCost="300" SaleCost="150"/>
+    <BrightHorn DepotSize="5" BuyCost="3000" SaleCost="1500"/>
+    <Intermediate DepotSize="4" BuyCost="4000" SaleCost="2000"/>
+    <Souvenir DepotSize="3" BuyCost="5000" SaleCost="2500"/>
+    <CheeseFerment DepotSize="2" BuyCost="25" SaleCost="15"/>
+    <Varnish DepotSize="2.5" BuyCost="25" SaleCost="15"/>
+    <MegaPie DepotSize="20" BuyCost="20000" SaleCost="10000"/>
+    <SpruceGrizzly DepotSize="25" BuyCost="2500" SaleCost="7000"/>
+    <SpruceLion DepotSize="25" BuyCost="2500" SaleCost="7000"/>
+    <SpruceBrownBear DepotSize="25" BuyCost="2500" SaleCost="7000"/>
+    <SpruceJaguar DepotSize="25" BuyCost="2500" SaleCost="7000"/>
+    <SpruceWhiteBear DepotSize="25" BuyCost="2500" SaleCost="7000"/>
+    <CagedGrizzly DepotSize="20" BuyCost="80" SaleCost="80"/>
+    <CagedJaguar DepotSize="20" BuyCost="200" SaleCost="200"/>
+    <CagedWhiteBear DepotSize="20" BuyCost="100" SaleCost="100"/>
+    */
+}
