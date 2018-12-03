2a83367972c965bd27796f805c03a0c4c4359eb0
fereshte <fereshteh.forghani2012@gmail.com>
2018-12-03 23:03:11 +0330

    cat move has been added.

--- src/Model/Animals/Cat.java
+++ src/Model/Animals/Cat.java
@@ -1,5 +1,6 @@
 package Model.Animals;
 
+import Model.DIRECTION;
 import Model.Map;
 
 public class Cat extends Animal {
@@ -11,5 +12,37 @@ public class Cat extends Animal {
     public void upgrade (){
         super.upgrade();
         //TODO complete
+    }
+
+    @Override
+    public void move() {
+        if(this.level == 1 || (this.level == 2 && !map.isThereItem())){
+            super.move();
+        }
+
+        else {
+            DIRECTION direction = map.findNearestItem((int)this.x , (int)this.y);
+            switch (direction){
+                case UP:{
+                    this.y -- ;
+                    break;
+                }
+                case DOWN:{
+                    this.y ++ ;
+                    break;
+                }
+                case RIGHT:{
+                    this.x ++ ;
+                    break;
+                }
+                case LEFT:{
+                    this.x -- ;
+                    break;
+                }
+                default:{
+                    break;
+                }
+            }
+        }
     }
 }
