998998bbde4e84ec0c20c0139dbfc0d32ad084a5
fereshte <fereshteh.forghani2012@gmail.com>
2018-12-03 22:55:38 +0330

    animal move() has been added.

--- src/Model/Animals/Animal.java
+++ src/Model/Animals/Animal.java
@@ -10,6 +10,50 @@ public abstract class Animal extends Entity {
         this.y = y;
     }
     public void move(){
+        int direction = (int)(Math.random() * 3 + 1);
 
+        // up direction
+        if(direction == 1){
+            if(this.y == 0){
+                // go down if you can't go up
+                this.y ++ ;
+            }
+            else{
+                this.y -- ;
+            }
+        }
+
+        // right direction
+        if(direction == 2){
+            if(this.x == map.getWidth()-1){
+                // go left if you can't go right
+                this.x -- ;
+            }
+            else{
+                this.x ++ ;
+            }
+        }
+
+        // down direction
+        if(direction == 3){
+            if(this.y == map.getLength()-1){
+                // go up if you can't go down
+                this.y -- ;
+            }
+            else{
+                this.y ++ ;
+            }
+        }
+
+        // left direction
+        if(direction == 4){
+            if(this.x == 0){
+                // go right if you can't go left
+                this.x ++ ;
+            }
+            else{
+                this.x -- ;
+            }
+        }
     }
 }
