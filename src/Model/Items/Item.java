bca9778ff4e6b99c4e1db00248f26c06400e3ac4
fereshte <fereshteh.forghani2012@gmail.com>
2018-12-06 15:27:34 +0330

    Item constructor has been written.

--- src/Model/Items/Item.java
+++ src/Model/Items/Item.java
@@ -2,4 +2,11 @@ package Model.Items;
 import Model.Entity;
 
 public class Item extends Entity {
+    String kind ;
+
+    Item(double x , double  y , String kind){
+        this.x = x ;
+        this.y = y ;
+        this.kind = kind ;
+    }
 }
