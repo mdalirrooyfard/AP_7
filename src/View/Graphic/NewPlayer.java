1aecbcea36030fd5805f09b94054ac1f228d04d5
mahsa <mahsadalir91@gmail.com>
2019-01-09 12:49:56 +0330

    New player -> get name from user

--- src/View/Graphic/NewPlayer.java
+++ src/View/Graphic/NewPlayer.java
@@ -4,6 +4,7 @@ import Model.Player;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -22,22 +23,24 @@ public class NewPlayer {
     public NewPlayer(Stage stage, int lastId){
         try {
             Group root = new Group();
-            Scene scene = new Scene(root, 200, 300);
+            Scene scene = new Scene(root, 300, 300);
             String style = this.getClass().getResource("graphic.css").toExternalForm();
             scene.getStylesheets().add(style);
             Stage newPlayerStage = new Stage();
-            newPlayerStage.setX(stage.getWidth() / 2);
-            newPlayerStage.setY(stage.getHeight() / 2);
-            Image image = new Image(new FileInputStream("src\\Resources\\Graphic\\Papirous.png"), 200, 300, false, true);
+            newPlayerStage.setX(stage.getWidth() / 2 - 200);
+            newPlayerStage.setY(stage.getHeight() / 2 - 200);
+            Image image = new Image(new FileInputStream("src\\Resources\\Graphic\\Papirous.png"), 300, 300, false, true);
             ImageView imageView = new ImageView(image);
             root.getChildren().add(imageView);
             Label label = new Label("Enter your Name");
             label.setId("NewPlayer");
             label.relocate(50, 50);
             TextField nameField = new TextField();
-            nameField.relocate(70, 70);
+            nameField.relocate(50, 100);
             nameField.setId("NameText");
             Button ok = new Button("ok");
+            ok.relocate(50, 140);
+            ok.setId("okButton");
             ok.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
@@ -46,9 +49,15 @@ public class NewPlayer {
                         player = new Player(name, lastId + 1);
                         newPlayerStage.close();
                     }
+                    else {
+                        Alert alert = new Alert(Alert.AlertType.WARNING);
+                        alert.setHeaderText("No player!");
+                        alert.setContentText("Enter your name");
+                        alert.showAndWait();
+                    }
                 }
             });
-            root.getChildren().addAll(label, nameField);
+            root.getChildren().addAll(label, nameField, ok);
             newPlayerStage.setScene(scene);
             newPlayerStage.show();
         }
