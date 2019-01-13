94a6787aa99bd47f6a71462314a7d7d9e5f27c56
fereshte1378 <fereshteh.forghani2012@gmail.com>
2019-01-13 13:28:41 +0330

    set the place of icons.

--- src/View/View.java
+++ src/View/View.java
@@ -1,7 +1,5 @@
 package View;
 
-import Controller.Controller;
-import Model.Farm;
 import View.Graphic.Menu;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -40,14 +38,35 @@ public class View
             ImageView backgroundView = new ImageView(background);
             backgroundView.setX(0);
             backgroundView.setY(0);
-            Circle circle = new Circle(50 , 65 , 30);
-            circle.setFill(Color.rgb(255 , 255 ,125));
-            Image henIcon = new Image(new FileInputStream("src\\Resources\\Graphic\\icons\\henIcon.png"),
-                    100 , 100 , false , true);
+            group.getChildren().addAll(backgroundView);
+            Circle henCircle = new Circle(35 , 42 , 30);
+            henCircle.setFill(Color.rgb(250 , 0 ,30));
+            group.getChildren().addAll(henCircle);
+            Image henIcon = new Image(new FileInputStream("src\\Resources\\Graphic\\UI\\Icons\\Products\\chicken.png"),
+                    60 , 60 , false , true);
             ImageView henIconView = new ImageView(henIcon);
-            henIconView.setY(30);
-            henIconView.setX(25);
-            group.getChildren().addAll(backgroundView,circle,henIconView);
+            henIconView.setX(5);
+            henIconView.setY(8);
+            group.getChildren().addAll(henIconView);
+            Circle cowCircle = new Circle(100 , 42 , 30);
+            cowCircle.setFill(Color.rgb(96 , 96 ,96));
+            group.getChildren().addAll(cowCircle);
+            Image cowIcon = new Image(new FileInputStream("src\\Resources\\Graphic\\UI\\Icons\\Products\\cow.png"),
+                    50 , 50 , false , true);
+            ImageView cowIconView = new ImageView(cowIcon);
+            cowIconView.setX(70);
+            cowIconView.setY(13);
+            group.getChildren().addAll(cowIconView);
+            Circle sheepCircle = new Circle(175 , 42 , 30);
+            sheepCircle.setFill(Color.rgb(225 , 153 ,224));
+            group.getChildren().addAll(sheepCircle);
+            Image sheepIcon = new Image(new FileInputStream("src\\Resources\\Graphic\\UI\\Icons\\Products\\sheep.png"),
+                    52, 52 , false , true);
+            ImageView sheepIconView = new ImageView(sheepIcon);
+            sheepIconView.setX(146);
+            sheepIconView.setY(10);
+            group.getChildren().addAll(sheepIconView);
+
         }
         catch (FileNotFoundException e) {}
     }
