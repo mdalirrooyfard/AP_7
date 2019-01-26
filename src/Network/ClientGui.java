94b6234fb461bee6081b90fba97901ffec2c7ceb
mahsa <mahsadalir91@gmail.com>
2019-01-26 22:52:46 +0330

    Client Sender and Client Gui.

--- src/Network/ClientGui.java
+++ src/Network/ClientGui.java
@@ -1,9 +1,56 @@
 package Network;
 
-public class ClientGui {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class ClientGui extends Application
+{
+    private TextArea chatArea = new TextArea();
     private ClientSender clientSender;
 
     public ClientGui(ClientSender clientSender){
         this.clientSender = clientSender;
     }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //primaryStage.setTitle(client.getName()+(isGroup?" in group":" pv to "+ friend));
+        //todo figure out how to put the name of player here
+        TextField textField = new TextField();
+        Button button = new Button("send");
+        chatArea.setFont(new Font(25));
+        chatArea.setWrapText(true);
+        HBox hBox = new HBox(20, chatArea);
+        HBox hBox1 = new HBox(20, textField, button);
+        VBox vBox = new VBox(20, hBox1, hBox);
+        Scene scene = new Scene(vBox);
+        button.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                String data = textField.getText();
+                if (!data.equals("")){
+                    clientSender.sendMessage(data);
+                    //todo esm player ham bayad bashe inja
+                }
+                textField.setText("");
+            }
+        });
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public  void putInCharArea(String message) {
+        chatArea.appendText(message + "\n");
+    }
+
 }
